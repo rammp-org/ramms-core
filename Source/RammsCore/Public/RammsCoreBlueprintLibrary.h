2d07ed459dcb0eccdb35bd373b9d762ab1d7005f
William Emfinger <waemfinger@gmail.com>
2026-04-15 09:25:00 -0500

    address comments

--- Source/RammsCore/Public/RammsCoreBlueprintLibrary.h
+++ Source/RammsCore/Public/RammsCoreBlueprintLibrary.h
@@ -5,14 +5,14 @@
 #include "RammsCoreBlueprintLibrary.generated.h"
 
 UCLASS()
-class URammsCoreBlueprintLibrary : public UBlueprintFunctionLibrary
+class RAMMSCORE_API URammsCoreBlueprintLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 public:
 	// Allow the blueprint to determine if this is with editor or not. Note: this
 	// is the same as RunningInPIE, but kept for semantic clarity in some cases
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsWithEditor()
 	{
 #if WITH_EDITOR
@@ -23,7 +23,7 @@ public:
 	};
 
 	// Allow the blueprint to determine whether the current world is running as PIE
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils", meta = (WorldContext = "WorldContextObject"))
 	static bool IsPIE(const UObject* WorldContextObject)
 	{
 #if WITH_EDITOR
@@ -40,7 +40,7 @@ public:
 
 	// Allow the blueprint to determine whether we are running outside of the
 	// editor (e.g. in a packaged build or standalone) or not
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils", meta = (WorldContext = "WorldContextObject"))
 	static bool IsOutsideEditor(const UObject* WorldContextObject)
 	{
 #if !WITH_EDITOR
@@ -63,7 +63,7 @@ public:
 	// excludes some non-editor builds (e.g. some test builds). Development
 	// builds are not packaged, but also not with editor, so this is a useful
 	// distinction.
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsPackagedBuild()
 	{
 #if UE_BUILD_SHIPPING || UE_BUILD_TEST
@@ -74,7 +74,7 @@ public:
 	};
 
 	// Allow the blueprint to determine whether this is a debug build or not
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsDebugBuild()
 	{
 #if UE_BUILD_DEBUG
@@ -85,7 +85,7 @@ public:
 	};
 
 	// Allow the blueprint to determine whether this is a development build or not
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsDevelopmentBuild()
 	{
 #if UE_BUILD_DEVELOPMENT
@@ -96,7 +96,7 @@ public:
 	};
 
 	// Allow the blueprint to determine whether this is a shipping build or not
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsShippingBuild()
 	{
 #if UE_BUILD_SHIPPING
@@ -107,7 +107,7 @@ public:
 	};
 
 	// Allow the blueprint to determine whether this is a test build or not
-	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "RammsCoreFunctionLibrary")
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "Ramms|Utils")
 	static bool IsTestBuild()
 	{
 #if UE_BUILD_TEST
